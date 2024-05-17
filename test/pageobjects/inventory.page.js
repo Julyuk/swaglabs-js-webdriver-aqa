aa15a1f7d6b95dac31eb37f1b7d4999b6adaeea8
Yuliia Ukrainets <ulaukrainec@gmail.com>
2024-05-17 23:43:43 +0300

    Fixed formatting in my code

--- test/pageobjects/inventory.page.js
+++ test/pageobjects/inventory.page.js
@@ -3,51 +3,50 @@ const Page = require('./page');
 
 
 class InventoryPage extends Page {
-    get btnCart(){
-        return $('#shopping_cart_container')
-     }
- clickBtnCart(){
-    this.btnCart.click()
-   }
-  get textHeading(){
-  return $('//div[@class="app_logo"]')
-    }
-get burgerMenu(){
-     return $('#react-burger-menu-btn')
-   }
+  get btnCart() {
+    return $("#shopping_cart_container");
+  }
+  clickBtnCart() {
+    this.btnCart.click();
+  }
+  get textHeading() {
+    return $('//div[@class="app_logo"]');
+  }
+  get burgerMenu() {
+    return $("#react-burger-menu-btn");
+  }
 
-    open () {
-        return super.open('inventory.html');
-    }
+  open() {
+    return super.open("inventory.html");
+  }
 
+  get items() {
+    return $$(`//div[@data-test="inventory-item"]`);
+  }
+  getItemById(id) {
+    return $(`(//div[@data-test="inventory-item"])[${id + 1}]`);
+  }
+  getItemImageById(id) {
+    return $(`(//div[@class="inventory_item_img"])[${id + 1}]`);
+  }
+  getItemPriceById(id) {
+    return $(`(//div[@class="inventory_item_price"])[${id + 1}]`);
+  }
+  getItemBtnById(id) {
+    return $(`(//button[contains(@name,"add-to-cart")])[${id + 1}]`);
+  }
+  getItemNameById(id) {
+    return $(`(//div[contains(@class,"item_name")])[${id + 1}]`);
+  }
+  getItemDescById(id) {
+    return $(`(//div[@class="inventory_item_desc"])[${id + 1}]`);
+  }
 
-    get items(){
-        return $$(`//div[@data-test="inventory-item"]`)
-    }
-    getItemById(id){
-        return $(`(//div[@data-test="inventory-item"])[${id+1}]`)
-    }
-    getItemImageById(id){
-        return $(`(//div[@class="inventory_item_img"])[${id+1}]`)
-    }
-    getItemPriceById(id){
-        return $(`(//div[@class="inventory_item_price"])[${id+1}]`)
-    }
-    getItemBtnById(id){
-        return $(`(//button[contains(@name,"add-to-cart")])[${id+1}]`)
-    }
-    getItemNameById(id){
-        return $(`(//div[contains(@class,"item_name")])[${id+1}]`)
-    }
-    getItemDescById(id){
-        return $(`(//div[@class="inventory_item_desc"])[${id+1}]`)
-    }
+  get productsText() {}
 
-    get productsText(){}
-
-    get dropdown(){}
-    get dropdownActiveOption(){}
-    clickDropdownOptionById(id){}
+  get dropdown() {}
+  get dropdownActiveOption() {}
+  clickDropdownOptionById(id) {}
 }
 
 module.exports = new InventoryPage();
--- test/specs/basic.js
+++ test/specs/basic.js
@@ -5,12 +5,16 @@ describe('Login page', () => {
     beforeEach(async () => {
         await loginPage.open();
         const title = await browser.getTitle();
-        await browser.waitUntil(async function () {
+        await browser.waitUntil(
+          async function () {
             return title === "Swag Labs";
-        }, {
+          },
+          {
             timeout: 5000,
-            timeoutMsg: 'expected text to be different after 5s and be strictly equal \'Swag Labs\''
-        });
+            timeoutMsg:
+              "expected text to be different after 5s and be strictly equal 'Swag Labs'",
+          }
+        );
         assert.strictEqual(title, "Swag Labs");
         const loginHeadingDisplayed = await (await loginPage.loginHeading).isDisplayed();
         assert.strictEqual(loginHeadingDisplayed, true);
